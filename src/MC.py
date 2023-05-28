72428ee5a11c0e6f40d5efd063df9d5f2d57c2fe
Simon Halstensen <simon.halstensen@gmail.com>
2023-05-28 16:32:45 +0200

    fixed reset bug

--- src/MC.py
+++ src/MC.py
@@ -7,7 +7,7 @@ from pqdm.processes import pqdm
 from scipy.sparse import csr_matrix
 from scipy.stats import norm
 
-def MC_step(z : RealArray, A : RealArray, L : RealArray, LT: RealArray, tol : float, mat=None) -> RealArray:
+def MC_step(z : RealArray, A : RealArray, L : RealArray, LT: RealArray, tol : float, W=None, WT=None) -> RealArray:
     """MC step: calculates (A^-1 @ z) * z 
 
     Args:
@@ -20,8 +20,8 @@ def MC_step(z : RealArray, A : RealArray, L : RealArray, LT: RealArray, tol : fl
         (RealArray): Result of the Monte Carlo step 
     """
     y = cg(A,z,L,LT,tol)
-    if mat is not None:
-        return y * z , (mat @ z) * z
+    if W is not None and WT is not None:
+        return y * z , (W @ (WT @ z)) * z
     else:
         return y * z
 
@@ -37,20 +37,13 @@ def MC(A: RealArray, L: RealArray, LT:RealArray, tol : float, N : int) -> RealAr
     Returns:
         (RealArray): Matrix of size (N,N_a) containing Monte Carlo estimate for each 1<n<N
     """
-    #z_mat=np.random.choice([-1,1],size=(A.shape[0],N))
-    #input=[[el, A , L, LT, tol, mat] for el in z_mat.T]
-    #print(cg(A, np.random.random(A.shape[0]),L, np.random.random(A.shape[0]), tol).shape,)
     ins = ((np.random.choice([-1,1],size=A.shape[0]), A, L, LT, tol) for _ in range(N))
     ret=np.array(pqdm(ins, MC_step, n_jobs=cpu_count(),argument_type='args'))
-    #ret = np.array([MC_step(el, A, L,LT, tol) for el in z_mat.T])
-    # ret=[]
-    # for ii in ins:
-    #     ret.append(MC_step(ii[0],ii[1],ii[2],ii[3],ii[4]))
     div=1/np.arange(1,N+1)
     return np.cumsum(ret,axis=1)*div[:,None]
     
 
-def MC_lanzos_control_variates(A: RealArray, L:RealArray, LT: RealArray, W_diag: RealArray, WWT: RealArray, N:int, tol: float, clip=False):
+def MC_lanzos_control_variates(A: RealArray, L:RealArray, LT: RealArray, W_diag: RealArray, W: RealArray, WT: RealArray,  N:int, tol: float, clip=False):
     """Parallel implementation of the combined Mc Lanczos method. Calculates an estimate of the diagonal of the inverse of the 
     matrix A for k Lanczos iterations and N MC iterations. Returns estimate with optimal variance control parameter
     and with fixed variance control parameter.
@@ -71,9 +64,9 @@ def MC_lanzos_control_variates(A: RealArray, L:RealArray, LT: RealArray, W_diag:
         mean_alphas (list(float)): List of size N containing the mean value of the matrix alpha at each MC step
     """
     #Sample Rademacher vectors to create input to parallel MC
-    ins = ((np.random.choice([-1,1],size=A.shape[0],replace=True), A, L, LT, tol,WWT) for _ in range(N))
+    ins = ((np.random.choice([-1,1],size=A.shape[0],replace=True), A, L, LT, tol, W, WT) for _ in range(N))
     #Run MC with variate reduction
-    ret=np.array(pqdm(ins, MC_step, n_jobs=cpu_count(),argument_type='args'))
+    ret=np.array(pqdm(ins, MC_step, n_jobs=cpu_count(), argument_type='args'))
     
     #We store MC estimates for 1<n<N 
     fixed=np.zeros((N,A.shape[0]))
@@ -83,7 +76,7 @@ def MC_lanzos_control_variates(A: RealArray, L:RealArray, LT: RealArray, W_diag:
     for i in range(1,N+1):
         est_mu_z = np.mean(ret[:i, 0], axis=0)
         est_mu_y = np.mean(ret[:i, 1], axis=0)
-        est_sigma_y = np.mean((ret[:i, 1] - W_diag) ** 2, axis=0) # TODO: We tecnically know this one already, but probably hard to compute
+        est_sigma_y = np.mean((ret[:i, 1] - W_diag) ** 2, axis=0)
         est_sigma_zy = np.mean((ret[:i, 0] - est_mu_z) * (ret[:i, 1] - W_diag), axis=0)
         alpha = est_sigma_zy / est_sigma_y
         if clip:
@@ -99,14 +92,11 @@ def MC_lanzos_control_variates(A: RealArray, L:RealArray, LT: RealArray, W_diag:
 
 
 
-def MC_lanzos_control_variates_2(Z: Callable[[RealArray], RealArray], Y: Callable[[RealArray], RealArray], mu_y: RealArray, N:int, n: int, clip=False):
+def MC_lanzos_control_variates_single_proc(Z: Callable[[RealArray], RealArray], Y: Callable[[RealArray], RealArray], mu_y: RealArray, N:int, n: int, clip=False):
     def mc_cv_step(Z: Callable[[RealArray], RealArray], Y: Callable[[RealArray], RealArray]):
         z = np.random.choice([-1,1],size=n, replace=True)
         return Z(z), Y(z)
-    #ins = ((Z, Y) for _ in range(N))
-    #ret=np.array(pqdm(ins, mc_cv_step, n_jobs=cpu_count(),argument_type='args'))
     ret = np.array([mc_cv_step(Z, Y) for _ in range(N)])
-    #ret = np.array([MC_step(el, A, L,LT, tol) for el in z_mat.T])
     est_mu_z = np.mean(ret[:, 0], axis=0)
     est_mu_y = np.mean(ret[:, 1], axis=0)
     est_sigma_y = np.mean((ret[:, 1] - mu_y) ** 2, axis=0) # TODO: We tecnically know this one already, but probably hard to compute
--- src/lanczos_MC.py
+++ src/lanczos_MC.py
@@ -10,7 +10,7 @@ def lanczos_MC(A,G,U,alpha,beta,k,N):
         
     GT = sparse.csr_matrix(G.T)
     
-    est_diag_comb, est_diag_comb_fix, optimal_alpha= MC_lanzos_control_variates(A, G,GT , est_diag_l, W @ W.T, N,1e-9, clip=True)
+    est_diag_comb, est_diag_comb_fix, optimal_alpha= MC_lanzos_control_variates(A, G,GT , est_diag_l, W @ W.T, N,1e-9, clip=False)
     #Z = lambda z: cg(A, z, G, GT, 1e-9) * z
     #Y = lambda z: (W @ (W.T @ z)) * z
     #est_diag_comb, est_diag_comb_fix= MC_lanzos_control_variates_2(Z, Y, est_diag_l, N, A.shape[0], clip=True)
